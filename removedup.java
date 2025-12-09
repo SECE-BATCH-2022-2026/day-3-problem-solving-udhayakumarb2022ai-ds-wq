fd2c5fec4ec0542c43ab19e19ac2cbf480548140
udhayakumarb2022ai-ds-wq <udhayakumar.b2022ai-ds@sece.ac.in>
2025-12-09 10:06:23 +0530

    comit

--- removedup.java
+++ removedup.java
@@ -1,16 +1,33 @@
 import java.io.*;
-class removedup{
-	public static void main(String args[]){
-	BufferReader br=new BufferReader(new InputStreamReader(System.in));
-	int s=Integer.parseInt(br.readLine());
-	String [] inp=new String[s];
-
-
+import java.util.*;
+import java.util.stream.*;
+class Main{
+	public static void main(String args[]) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String [] inp = new String[n];
+		for(int i=0;i<n;i++){
+			inp[i] = br.readLine();
+		}
+		String [] op = UserMainCode.removeDuplicates(inp);
+		for(String s:op){
+			System.out.println(s);
+		}
+		br.close();
+		
 	}
 }
-class UserMainCode{
-	public static String[] removeduplicates(String [] inp){
-		//logic
+class UserMainCode {
+	public static String [] removeDuplicates (String [] inp){
+		Set<String> ts = new TreeSet<String>();
+		for(String s:inp){
+			ts.add(s);
 		}
-}
+		String[] res = new String[ts.size()];
+		int i=0;
+		for(String s:ts){
+			res[i++] = s;
+		} 
 
+		return res;
+	}
